92e1ad148dbd2f0077d14b6f3c3f626182ee046f
TaleRG <talesrodriguesgoncalves98@gmail.com>
2024-06-13 01:13:17 -0300

    feature:(adiciona endpoint listartodos percursos)

--- backend/veiculo-pi1/src/veiculo/controller/carro.controller.ts
+++ backend/veiculo-pi1/src/veiculo/controller/carro.controller.ts
@@ -1,7 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CarroRepository } from '../repository/carro.repository';
 import { DadosCarroDto } from '../dto/DadosCarro.dto';
-import { DadosVeicularesEntity } from '../entity/dadosVeiculares.entity';
 import { CarroService } from '../service/carro.service';
 
 @Controller('/percurso-carro')
@@ -9,14 +7,12 @@ export class CarroController {
   constructor(private carroService: CarroService) {}
   @Post()
   async criarPercuso(@Body() dadosPercurso: DadosCarroDto) {
-    const usuarioEntity = new DadosVeicularesEntity();
-    usuarioEntity.velocidade = dadosPercurso.velocidade;
-    usuarioEntity.aceleracao = dadosPercurso.aceleracao;
-    usuarioEntity.tempo = dadosPercurso.tempo;
-    usuarioEntity.consumo_energetico = dadosPercurso.consumo_energetico;
-    usuarioEntity.numero_percurso = dadosPercurso.numero_percurso;
+    await this.carroService.criarPercuso(dadosPercurso);
+    return dadosPercurso;
+  }
 
-    await this.carroService.criarPercuso(usuarioEntity);
-    return usuarioEntity;
+  @Get()
+  async listarPercursos() {
+    return this.carroService.listarTodosPercursos();
   }
 }
